3500a01a87b30d56124d8312a754641ea97ca475
robbedec <robbe.decorte@student.hogent.be>
2019-11-30 10:13:02 -0800

    feat: user can remove entry

--- calorietracker/View controllers/LogTableViewController.swift
+++ calorietracker/View controllers/LogTableViewController.swift
@@ -81,17 +81,21 @@ class LogTableViewController: UITableViewController {
     }
     */
 
-    /*
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            foodEntries.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
