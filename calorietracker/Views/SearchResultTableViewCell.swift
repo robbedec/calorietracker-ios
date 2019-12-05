c27681e52cda0250f154ec5cb67004fb16d3cf60
robbedec <robbe.decorte@student.hogent.be>
2019-12-05 12:56:04 -0800

    chore: custom tablecell

---
+++ calorietracker/Views/SearchResultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultTableViewCell.swift
+//  calorietracker
+//
+//  Created by ROBBE DECORTE on 05/12/2019.
+//  Copyright © 2019 ROBBE DECORTE. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
